b8125111a2082a3161f95e84a06ea21dd4781e45
Henrik Maler <hmaler@uni-osnabrueck.de>
2021-06-04 14:28:50 +0200

    2nd correction

--- 06_main.js
+++ 06_main.js
@@ -25,12 +25,12 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "179",
-            serverAppURL: "magpie-demo.herokuapp.com/api/submit_experiment/179",
+            experimentID: "180",
+            serverAppURL: "magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "directLink",
+            deployMethod: "debug",
             contact_email: "hmaler@uos.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
